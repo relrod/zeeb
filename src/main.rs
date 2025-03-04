e1b7da2e5e31287d6f708bc8e7650a94eacf3cae
Rick Elrod <rick@elrod.me>
2025-03-04 04:22:19 +0100

    Slight refactor to dedent a bunch of code
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- src/main.rs
+++ src/main.rs
@@ -174,65 +174,71 @@ fn drag_tile(
     camera_query: Query<(&Camera, &GlobalTransform)>,
     mut board: ResMut<BoardState>,
 ) {
+    let Ok((camera, camera_transform)) = camera_query.get_single() else {
+        return;
+    };
+
     for event in events.read() {
-        let (camera, camera_transform) = camera_query.single();
-        if let Ok(world_position) = camera.viewport_to_world_2d(camera_transform, event.position) {
-            for (entity, mut draggable, mut transform) in query.iter_mut() {
-                if mouse_input.pressed(MouseButton::Left) && draggable.is_dragging {
-                    transform.translation = world_position.extend(transform.translation.z);
-                    break;
+        let Ok(world_position) = camera.viewport_to_world_2d(camera_transform, event.position)
+        else {
+            continue;
+        };
+
+        for (entity, mut draggable, mut transform) in query.iter_mut() {
+            if mouse_input.pressed(MouseButton::Left) && draggable.is_dragging {
+                transform.translation = world_position.extend(transform.translation.z);
+                break;
+            }
+
+            if mouse_input.just_pressed(MouseButton::Left) {
+                let tile_position = transform.translation.xy();
+                if tile_position.distance(world_position) < TILE_SIZE / 2.0 {
+                    draggable.is_dragging = true;
                 }
+            }
 
-                if mouse_input.just_pressed(MouseButton::Left) {
-                    let tile_position = transform.translation.xy();
-                    if tile_position.distance(world_position) < TILE_SIZE / 2.0 {
-                        draggable.is_dragging = true;
-                    }
-                }
+            if mouse_input.just_released(MouseButton::Left) && draggable.is_dragging {
+                draggable.is_dragging = false;
 
-                if mouse_input.just_released(MouseButton::Left) && draggable.is_dragging {
-                    draggable.is_dragging = false;
-
-                    // If we are underneath the board, it's a free-for-all, don't snap to grid
-                    if world_position.y < -BOARD_CENTER {
-                        // But we still need to free up the grid cell if the tile was on the board
-                        if draggable.is_on_board {
-                            let (col, row) =
-                                BoardState::closest_cell_to_world(draggable.last_grid_position);
-                            board.remove_tile(col, row);
-                        }
-                        draggable.is_on_board = false;
-                        continue;
-                    }
-
-                    let (col, row) = BoardState::closest_cell_to_world(world_position);
-
-                    if board.is_occupied(col, row) {
-                        // If the cell we're trying to move into is occupied, move the tile back
-                        transform.translation =
-                            draggable.last_grid_position.extend(transform.translation.z);
-                        // Nothing changed, so don't update the board state.
-                    } else {
-                        // Otherwise it's a free cell, so figure out the world position of the cell
-                        // and move the tile there.
-                        let cell_center_world = BoardState::world_from_xy(col, row);
-                        transform.translation = cell_center_world.extend(transform.translation.z);
-
-                        // Update internal state so we know if another tile can move here
-                        board.place_tile(col, row, entity);
-                        draggable.is_on_board = true;
-
-                        // Then free up the old position
-                        let (old_col, old_row) =
+                // If we are underneath the board, it's a free-for-all, don't snap to grid
+                if world_position.y < -BOARD_CENTER {
+                    // But we still need to free up the grid cell if the tile was on the board
+                    if draggable.is_on_board {
+                        let (col, row) =
                             BoardState::closest_cell_to_world(draggable.last_grid_position);
-                        board.remove_tile(old_col, old_row);
-
-                        // This is what we'll snap the tile back to, if the player tries to
-                        // move it to an occupied cell in the future. Do this after we free up
-                        // the old position, since we use the old value to determine where the
-                        // tile came from.
-                        draggable.last_grid_position = cell_center_world;
+                        board.remove_tile(col, row);
                     }
+                    draggable.is_on_board = false;
+                    continue;
+                }
+
+                let (col, row) = BoardState::closest_cell_to_world(world_position);
+
+                if board.is_occupied(col, row) {
+                    // If the cell we're trying to move into is occupied, move the tile back
+                    transform.translation =
+                        draggable.last_grid_position.extend(transform.translation.z);
+                    // Nothing changed, so don't update the board state.
+                } else {
+                    // Otherwise it's a free cell, so figure out the world position of the cell
+                    // and move the tile there.
+                    let cell_center_world = BoardState::world_from_xy(col, row);
+                    transform.translation = cell_center_world.extend(transform.translation.z);
+
+                    // Update internal state so we know if another tile can move here
+                    board.place_tile(col, row, entity);
+                    draggable.is_on_board = true;
+
+                    // Then free up the old position
+                    let (old_col, old_row) =
+                        BoardState::closest_cell_to_world(draggable.last_grid_position);
+                    board.remove_tile(old_col, old_row);
+
+                    // This is what we'll snap the tile back to, if the player tries to
+                    // move it to an occupied cell in the future. Do this after we free up
+                    // the old position, since we use the old value to determine where the
+                    // tile came from.
+                    draggable.last_grid_position = cell_center_world;
                 }
             }
         }
